97d4845a1546a332e2dffa0352900efc0108e3a7
Gabriela Muniz <gabrielanamu@live.com>
2023-01-17 15:11:11 -0300

    project-trivia-req-20-b

--- src/tests/ranking.test.js
+++ src/tests/ranking.test.js
@@ -1,24 +1,30 @@
 import React from 'react';
-import { screen } from '@testing-library/react';
+import { screen, waitForElementToBeRemoved } from '@testing-library/react';
 import renderWithRouterAndRedux from './helpers/renderWithRouterAndRedux';
 import userEvent from '@testing-library/user-event';
+import { storageMock } from './helpers/data';
 import App from '../App';
 import Login from '../pages/Login';
 import Ranking from '../pages/Ranking';
+import { act } from 'react-dom/test-utils';
 
 describe('teste da página de ranking', () => {
-    test('Verifica se existe um botão que redireciona a pessoa para a tela inicial (login)', () => {
+    test('Verifica se existe um botão que redireciona a pessoa para a tela inicial (login)', async () => {
         const { history } = renderWithRouterAndRedux(<Ranking />);
-        const button = screen.getByTestId('btn-go-home');
-        expect(button).toBeInTheDocument();
-        userEvent.click(button);
+        const buttonBack = screen.getByTestId('btn-go-home');
+        expect(buttonBack).toBeInTheDocument();
+        userEvent.click(buttonBack);
         expect(history.location.pathname).toBe('/');
     });
-    test('', () => {
-        renderWithRouterAndRedux(<Ranking />);
-        const player = screen.getByTestId('player-name-0');
-        const score = screen.getByTestId('player-score-0');
-        expect(player).toBeInTheDocument();
-        expect(score).toBeInTheDocument(); 
+    test('Verifica se ranking de jogadores está na tela', () => {
+        const { history } = renderWithRouterAndRedux(<App />);
+        Object.defineProperty(window, 'localStorage', { value: storageMock });
+        act(() => history.push('/ranking'));
+        const player01 = screen.getByTestId('player-name-0');
+        const score01 = screen.getByTestId('player-score-0');
+        expect(player01).toBeInTheDocument();
+        expect(score01).toBeInTheDocument(); 
+        expect(screen.getByTestId('player-name-1')).toBeInTheDocument();
+        expect(screen.getByTestId('player-name-2')).toBeInTheDocument();
     })
 })
